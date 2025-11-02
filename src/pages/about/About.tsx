47226ad74ee53f9e5b1223c848279296d161ee1f
tianyao <ty18710388929@163.com>
2025-11-02 18:59:40 +0800

    add email

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -3,9 +3,9 @@ import './About.css';
 function About() {
   return (
     <div className="about-container">
-      <h2 className="about-title">About Page - 这是关于我们页面。</h2>
+      <h2 className="about-title">ty13363959807@gmail.com</h2>
     </div>
   );
-};
+}
 
 export default About;
